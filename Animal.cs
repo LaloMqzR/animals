32caaf63b4923f08be17e5392c66f918b655491a
LaloMqzR <eduardo.marquez@upaep.mx>
2026-04-28 07:35:18 -0600

    Add Animal class with attributes and methods

--- Animal.cs
+++ Animal.cs
@@ -1,1 +1,28 @@
+using System;
 
+class Animal 
+{
+   //Atributos 
+    public string Nombre;
+    public int Edad; 
+
+    //Constructor
+    public Animal (string nombre, int edad)
+    {
+        //Parametros 
+        Nombre = nombre;
+        Edad = edad;
+    }
+
+    //Método
+    public void HacerSonido()
+    {
+        Console.WriteLine (Nombre + " hace un sonido. ");
+    }
+
+    //Método para mostar info
+    public void MostrarInfo()
+    {
+        Console.WriteLine ("Nombre: " + Nombre + ", Edad: " + Edad);
+    }
+}
